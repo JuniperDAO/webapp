6282df971d343792b23615957e24ab478fa42343
David Young <dave@tungstenfi.com>
2024-11-04 11:10:40 -0700

    no gcp buckets on vercel

--- next.config.js
+++ next.config.js
@@ -4,25 +4,6 @@ const IS_DEV = process.env.NODE_ENV === 'development' // !['main', 'staging'].in
 module.exports = {
     reactStrictMode: false,
 
-    // Use the CDN in production, staging, and other GCP/run servers.
-    // Note that "*.wfi.dev" behaves just as production
-    assetPrefix: !IS_DEV ? `https://storage.googleapis.com/${process.env.BUCKET_NAME}/${process.env.SHORT_SHA}` : undefined,
-
-    // what is this?
-    images: {
-        domains: ['storage.googleapis.com'], // Add the domain name here
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'storage.googleapis.com',
-                port: '',
-                pathname: '**',
-            },
-        ],
-    },
-    env: {
-        // NEXT_PUBLIC...
-    },
     async headers() {
         return [
             {
@@ -48,44 +29,4 @@ module.exports = {
             },
         ]
     },
-}
-
-// Injected content via Sentry wizard below
-if (!IS_DEV) {
-    const { withSentryConfig } = require('@sentry/nextjs')
-
-    module.exports = withSentryConfig(
-        module.exports,
-        {
-            // For all available options, see:
-            // https://github.com/getsentry/sentry-webpack-plugin#options
-
-            // Suppresses source map uploading logs during build
-            silent: false,
-            org: 'tungsten-financial-inc',
-            project: IS_PRODUCTION ? 'juniper-main' : 'juniper-staging',
-        },
-        {
-            // For all available options, see:
-            // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-            // Upload a larger set of source maps for prettier stack traces (increases build time)
-            widenClientFileUpload: true,
-
-            // Transpiles SDK to be compatible with IE11 (increases bundle size)
-            transpileClientSDK: true,
-
-            // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-            tunnelRoute: '/monitoring',
-
-            // Hides source maps from generated client bundles
-            hideSourceMaps: true,
-
-            // Automatically tree-shake Sentry logger statements to reduce bundle size
-            disableLogger: true,
-
-            // https://docs.sentry.io/api/auth/
-            authToken: process.env.SENTRY_AUTH_TOKEN,
-        }
-    )
 }
